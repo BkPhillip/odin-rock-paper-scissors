7965cbe3bb23e8a661cd1f0e067bcb5a0102de49
Bryson Phillip <bkphillip@icloud.com>
2023-06-06 11:15:21 -0700

    Add game function to run a set number of rounds

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,37 @@
+game();
+
+function game() {
+  const gameRounds = 5;
+  let currentRound = 0;
+  let playerWins = 0;
+  let computerWins = 0;
+  
+  // Play 5 rounds of game. Ties are not counted as rounds. Game will not end if player or computer reach 3 wins despite game results being determined
+  do {
+    const playerSelection = prompt("Rock, paper, or scissors? Which do you choose?");
+    const computerSelection = getComputerChoice();
+    
+    const gameResults = playRound(playerSelection, computerSelection);
+    console.log(gameResults);
+    //Check if return string contains the word win or lose
+    if (gameResults.split(" ").includes("Win!")) {
+      playerWins += 1;
+      currentRound += 1;
+    } else if (gameResults.split(" ").includes("Lose!")) {
+      computerWins += 1;
+      currentRound += 1;
+    } 
+    console.log("Player: " + playerWins + " Computer: " + computerWins)
+  } while (currentRound < gameRounds);
+  //Display  final results
+  if (playerWins > computerWins) {
+    console.log("You Win!");
+  } else {
+    console.log("You Lose!")
+  }
+  console.log("Final Score: Player " + playerWins + " | Computer " + computerWins);
+}
+
 function getComputerChoice() {
   const gameArray = ['rock', 'paper', 'scissors'];
   const randomElement = gameArray[Math.floor(Math.random() * gameArray.length)];
@@ -5,6 +39,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
   //First check if the two selections are identical to check for tie
   //If selections not identical, then compare the players selection with the computer selection in switch case
   const upperPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
@@ -14,7 +49,7 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "Tie. Both players choose " + upperPlayerSelection;
   }
-  
+
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "scissors") {
